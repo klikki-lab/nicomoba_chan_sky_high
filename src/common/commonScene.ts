1050bea8d0c15f5ed15ccc6fedb4c0c99d22f441
klikki-lab <klikki.lab@gmail.com>
2024-09-03 17:22:25 +0900

    commonScene.tsの追加

--- src/common/commonScene.ts
+++ src/common/commonScene.ts
@@ -1,6 +1,6 @@
 export abstract class CommonScene extends g.Scene {
 
-    private _onFinish?: () => void;
+    protected _onFinish?: () => void;
 
     set onFinish(callback: () => void) { this._onFinish = callback; }
 }
